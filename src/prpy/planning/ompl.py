19f1ebee41e785b74aea236cd36390bd3bdf4aa8
Michael Koval <mkoval@cs.cmu.edu>
2015-04-24 20:45:59 -0400

    Added support for PR_RRTConnect.

--- src/prpy/planning/ompl.py
+++ src/prpy/planning/ompl.py
@@ -54,6 +54,9 @@ class OMPLPlanner(BasePlanner):
     def __str__(self):
         return 'OMPL {0:s}'.format(self.algorithm)
 
+    def GetDefaultArguments(self, robot):
+        return dict()
+
     @PlanningMethod
     def PlanToConfiguration(self, robot, goal, **kw_args):
         """
@@ -78,15 +81,23 @@ class OMPLPlanner(BasePlanner):
     def _Plan(self, robot, goal=None, timeout=30., shortcut_timeout=5.,
               continue_planner=False, ompl_args=None, 
               formatted_extra_params=None, **kw_args):
-        extraParams = '<time_limit>{:f}</time_limit>'.format(timeout)
+
+        # Create the dictionary of properties to add to PlannerParameters.
+        all_args = self.GetDefaultArguments(robot)
+        all_args['time_limit'] = timeout
 
         if ompl_args is not None:
-            for key,value in ompl_args.iteritems():
-                extraParams += '<{k:s}>{v:s}</{k:s}>'.format(k=str(key), v=str(value))
+            all_args.update(ompl_args)
+
+        # Serialize the parameters to XML.
+        extraParams = ''
+        for key,value in all_args.iteritems():
+            extraParams += '<{k:s}>{v:s}</{k:s}>'.format(k=str(key), v=str(value))
 
         if formatted_extra_params is not None:
             extraParams += formatted_extra_params
 
+        # Create the PlannerParameters struct.
         params = openravepy.Planner.PlannerParameters()
         params.SetRobotActiveJoints(robot)
         if goal is not None:
@@ -114,7 +125,6 @@ class OMPLPlanner(BasePlanner):
         return traj
 
     def _TSRPlan(self, robot, tsrchains, **kw_args):
-        
         extraParams = ''
         for chain in tsrchains:
             extraParams += '<{k:s}>{v:s}</{k:s}>'.format(k = 'tsr_chain', v=chain.serialize())
@@ -123,66 +133,45 @@ class OMPLPlanner(BasePlanner):
 
 
 class RRTConnect(OMPLPlanner):
-    def __init__(self):
-        OMPLPlanner.__init__(self, algorithm='RRTConnect')
+    def __init__(self, algorithm='RRTConnect'):
+        OMPLPlanner.__init__(self, algorithm=algorithm)
 
-    def _SetPlannerRange(robot, ompl_args=None):
-        from copy import deepcopy
+    def GetDefaultArguments(self, robot):
+        epsilon = 1e-6
 
-        if ompl_args is None:
-            ompl_args = {}
-        else:
-            ompl_args = deepcopy(ompl_args)
+        # Compute the collision checking resolution as a conservative
+        # fraction of the state space extents. This mimics the logic inside
+        # or_ompl used to set collision checking resolution.
+        dof_limit_lower, dof_limit_upper = robot.GetActiveDOFLimits()
+        dof_ranges = dof_limit_upper - dof_limit_lower
+        dof_resolution = robot.GetActiveDOFResolutions()
+        ratios = dof_resolution / dof_ranges
+        conservative_ratio = numpy.min(ratios)
 
-        # Set the default range to the collision checking resolution.
-        if 'range' not in ompl_args:
-            epsilon = 1e-6
+        # Convert the ratio back to a collision checking resolution. Set
+        # RRT-Connect's range to the same value used for collision
+        # detection inside OpenRAVE.
+        longest_extent = numpy.max(dof_ranges)
+        ompl_range = conservative_ratio * longest_extent - epsilon
 
-            # Compute the collision checking resolution as a conservative
-            # fraction of the state space extents. This mimics the logic inside
-            # or_ompl used to set collision checking resolution.
-            dof_limit_lower, dof_limit_upper = robot.GetActiveDOFLimits()
-            dof_ranges = dof_limit_upper - dof_limit_lower
-            dof_resolution = robot.GetActiveDOFResolutions()
-            ratios = dof_resolution / dof_ranges
-            conservative_ratio = numpy.min(ratios)
+        logger.debug('Computed RRT-Connect range parameter of %.3f.',
+                     ompl_range)
 
-            # Convert the ratio back to a collision checking resolution. Set
-            # RRT-Connect's range to the same value used for collision
-            # detection inside OpenRAVE.
-            longest_extent = numpy.max(dof_ranges)
-            ompl_range = conservative_ratio * longest_extent - epsilon
+        # Merge with any other default arguments.
+        params = super(RRTConnect, self).GetDefaultArguments(robot)
+        params['range'] = ompl_range
+        return params
 
-            ompl_args['range'] = ompl_range
-            logger.debug('Defaulted RRT-Connect range parameter to %.3f.',
-                         ompl_range)
-        return ompl_args
 
-    @PlanningMethod
-    def PlanToConfiguration(self, robot, goal, ompl_args=None, **kw_args):
-        """
-        Plan to a desired configuration with OMPL. This will invoke the OMPL
-        planner specified in the OMPLPlanner constructor.
-        @param robot
-        @param goal desired configuration
-        @return traj
-        """
-        
-        ompl_args = self._SetPlannerRange(robot, ompl_args=ompl_args)
-        return self._Plan(robot, goal=goal, ompl_args=ompl_args, **kw_args)
+class PR_RRTConnect(RRTConnect):
+    def __init__(self, algorithm='PR_RRTConnect', extension_type='con-con'):
+        RRTConnect.__init__(self, algorithm=algorithm)
+        self.extension_type = extension_type
 
-    @PlanningMethod
-    def PlanToTSR(self, robot, tsrchains, ompl_args=None, **kw_args):
-        """
-        Plan using the given TSR chains with OMPL. 
-        @param robot
-        @param tsrchains A list of TSRChain objects to respect during planning
-        @param ompl_args ompl RRTConnect specific parameters
-        @return traj
-        """
-        
-        ompl_args = self._SetPlannerRange(robot, ompl_args=ompl_args)
-        return self._TSRPlan(robot, tsrchains, ompl_args=ompl_args, **kw_args)
+    def GetDefaultArguments(self, robot):
+        params = super(PR_RRTConnect, self).GetDefaultArguments(robot)
+        params['extension_type'] = self.extension_type
+        return params
 
 
 class OMPLSimplifier(BasePlanner):
